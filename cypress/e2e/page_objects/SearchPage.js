32fed9f695a2444b8bf50e5afdee01210769a8ab
kuboslav82 <31851162+kuboslav82@users.noreply.github.com>
2024-04-29 12:17:42 +0200

    Delete cypress directory

--- cypress/e2e/page_objects/SearchPage.js
+++
@@ -1,7 +0,0 @@
-export default class searchPage {
-    
-    static visitSearchPage() {
-        cy.visit("https://staging.christies.com/en/search?entry=picasso&page=1&sortby=relevance&tab=available_lots")
-    }
-    
-}
